b507f827f7c4e3b0fca50c1f98386dab96557af9
clementdufour94 <clement.dufourl@gmail.com>
2023-12-01 12:39:43 +0100

    ModifHome

--- src/components/Musics.js
+++ src/components/Musics.js
@@ -3,35 +3,35 @@ import axios from 'axios';
 import {Link} from 'react-router-dom';
 import {Container, Row, Col, Card} from 'react-bootstrap';
 
-const Albums = () => {
-  const [albums, setalbums] = useState([]);
+const Musics = () => {
+  const [musics, setmusics] = useState([]);
 
   useEffect(() => {
-    const fetchalbums = async () => {
+    const fetchmusics = async () => {
       try {
-        const response = await axios.get('http://localhost:3001/api/albums');
-        setalbums(response.data);
+        const response = await axios.get('http://localhost:3001/api/musics');
+        setmusics(response.data);
       } catch (error) {
         console.error('Une erreur est survenue', error);
       }
     };
 
-    fetchalbums();
+    fetchmusics();
   }, []);
 
   return (
     <Container>
-      <h1 className="text-center my-4">Tout les albums</h1>
+      <h1 className="text-center my-4">Toute les musiques</h1>
       <Row className="justify-content-center">
-        {albums.map((album, index) => (
+        {musics.map((music, index) => (
           <Col xs={6} md={3} key={index} className="mb-4">
             <Card
               as={Link}
-              to={`/album/edit/${album.id}`}
+              to={`/music/edit/${music.id}`}
               style={{cursor: 'pointer'}}>
               <Card.Img variant="top" src="https://via.placeholder.com/150" />
               <Card.Body>
-                <Card.Title className="text-center">{album.title}</Card.Title>
+                <Card.Title className="text-center">{music.title}</Card.Title>
               </Card.Body>
             </Card>
           </Col>
@@ -41,4 +41,4 @@ const Albums = () => {
   );
 };
 
-export default Albums;
+export default Musics;
